7948ace6308d6244a3a56f857b3ca641590e06d0
ali-a-alwan <156842868+ali-a-alwan@users.noreply.github.com>
2024-03-22 12:22:16 -0400

    Add files via upload

---
+++ RecipeDetailsScreen.js
@@ -0,0 +1,22 @@
+//Complete the code for the RecipeDetailsScreen 
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 5,
+  },
+});
+
+export default RecipeDetailsScreen;
